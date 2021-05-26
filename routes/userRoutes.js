e11bcd25305484a2ee289d3996ba8078fb0e6dc2
PawaN-K-MishrA <pawanmishra01031999@gmail.com>
2021-05-26 13:59:55 +0530

    commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,5 @@ const router = express.Router();
 router.post("/signUp", userController.signUp);
 router.post("/login", userController.login);
 //router.post("/confrimEmail/:token", userController.confirmEmail);
-router.post("/activeUser", userController.protect, userController.activeUser);
+// router.post("/activeUser", userController.protect, userController.activeUser);
 module.exports = router;
